e8c4b8482ebcb050de2b74d8fd418422bc721cd7
N13u0W <joeloesteenkimberley@gmail.com>
2026-04-06 08:13:41 +0700

    second commit

--- main.py
+++ main.py
@@ -1,13 +1,25 @@
 from selenium import webdriver
-from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.common.by import By
 from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.chrome.service import Service
+import time
+
+print("Starting test...")
 
 driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
 
+driver.get("https://demo-webv1.vercel.app/")
 
-print("Starting browser...")
+time.sleep(2)
 
-driver.get("https://www.google.com")
+driver.find_element(By.ID, "username").send_keys("admin")
+driver.find_element(By.ID, "password").send_keys("1234")
+driver.find_element(By.TAG_NAME, "button").click()
+
+time.sleep(2)
+
+message = driver.find_element(By.ID, "message").text
+print("Result:", message)
 
 input("Press Enter to close...")
 driver.quit()
