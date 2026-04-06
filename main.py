d31fac06658e222dc1e167b368fbbe7de5ef697c
N13u0W <joeloesteenkimberley@gmail.com>
2026-04-06 07:42:46 +0700

    first commit

---
+++ main.py
@@ -0,0 +1,13 @@
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from webdriver_manager.chrome import ChromeDriverManager
+
+driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
+
+
+print("Starting browser...")
+
+driver.get("https://www.google.com")
+
+input("Press Enter to close...")
+driver.quit()
